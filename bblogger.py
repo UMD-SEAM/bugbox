6640f9f3e546feb202e07ba0ff6a3b6aaaf7ecf4
Samurai User <root@debian6>
2013-05-20 12:52:44 -0400

    working on tests

---
+++ bblogger.py
@@ -0,0 +1,42 @@
+import logging
+
+
+
+class ColoredFormatter(logging.Formatter):
+
+    def __init__(self, msg):
+        logging.Formatter.__init__(self, msg)
+        
+        self.COLORS = {
+            'WARNING' :  '\033[33m',
+            'INFO' :     '\033[34m',
+            'DEBUG' :    '\033[34m',
+            'CRITICAL' : '\033[33m',
+            'ERROR' :    '\033[91m',
+            'OK' :       '\033[92m',
+            'GRAY' :     '\033[90m',
+            'ULINE' :    '\033[4m',
+            'ENDC' :     '\033[0m'
+            }
+        return
+
+    def format(self, record):
+        levelname = record.levelname
+        if levelname in self.COLORS:
+            levelname_color = "%s%s%s" % (self.COLORS[levelname], 
+                                          levelname, 
+                                          self.COLORS['ENDC'])
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
+
+
+# setup logging
+OKLVL = 22
+logging.addLevelName(OKLVL,'OK')
+logger = logging.getLogger('') # root level logger
+logger.setLevel(logging.INFO)
+formatter = ColoredFormatter("[%(levelname)s] <%(name)s> %(message)s")
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+ch.setLevel(logging.INFO)
--- bbmanage.py
+++ bbmanage.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
 import sys
+import bblogger
 import logging
+
+logger = logging.getLogger("bbmanage")
 
 ULINE = '\033[4m'
 ENDC = '\033[0m'
@@ -19,46 +22,6 @@ def usage():
            "\ttrace_on\t<exploit_name>\n"                               \
            "\ttrace_off\t<exploit_name>\n"                              \
            "\tautorun\t\t<exploit_name>"
-
-
-class ColoredFormatter(logging.Formatter):
-
-    def __init__(self, msg):
-        logging.Formatter.__init__(self, msg)
-        
-        self.COLORS = {
-            'WARNING' :  '\033[33m',
-            'INFO' :     '\033[34m',
-            'DEBUG' :    '\033[34m',
-            'CRITICAL' : '\033[33m',
-            'ERROR' :    '\033[91m',
-            'OK' :       '\033[92m',
-            'GRAY' :     '\033[90m',
-            'ULINE' :    '\033[4m',
-            'ENDC' :     '\033[0m'
-            }
-        return
-
-    def format(self, record):
-        levelname = record.levelname
-        if levelname in self.COLORS:
-            levelname_color = "%s%s%s" % (self.COLORS[levelname], 
-                                          levelname, 
-                                          self.COLORS['ENDC'])
-            record.levelname = levelname_color
-        return logging.Formatter.format(self, record)
-
-# setup logging
-OKLVL = 22
-logging.addLevelName(OKLVL,'OK')
-logger = logging.getLogger('') # root level logger
-logger.setLevel(logging.INFO)
-formatter = ColoredFormatter("[%(levelname)s] <%(name)s> %(message)s")
-ch = logging.StreamHandler()
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-ch.setLevel(logging.INFO)
-logger = logging.getLogger("bbmanage")
 
 if __name__ == "__main__":
     
