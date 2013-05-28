563bf92e8a85dd0e1d63558d5ac0fe4414a03d63
Gary Nilson <gnilson@gmail.com>
2013-05-28 10:19:03 -0400

    Change design of Target modules to allow for Java service vulnerabilities

--- framework/Targets/drupal_6_14/__init__.py
+++ framework/Targets/drupal_6_14/__init__.py
@@ -1,10 +1,13 @@
 import os
+from framework.Targets import ApacheTarget
 
-def get_path(filename):
-    return os.path.dirname(os.path.realpath(__file__)) + '/' + filename
+class Target(ApacheTarget):
 
-name = "Drupal 6.14"
-application_dir_mapping = [get_path("application"), "/var/www"]
-database_filename = get_path("database.sql")
-database_name = "drupal_6_14"
-chroot_environment = "Debian7"
+    def get_path(filename):
+        return os.path.dirname(os.path.realpath(__file__)) + '/' + filename
+    
+    name = "Drupal 6.14"
+    application_dir_mapping = [get_path("application"), "/var/www"]
+    database_filename = get_path("database.sql")
+    database_name = "drupal_6_14"
+    chroot_environment = "Debian7"
--- framework/Targets/horde_3_3_12/__init__.py
+++ framework/Targets/horde_3_3_12/__init__.py
@@ -1,10 +1,13 @@
 import os
+from framework.Targets import ApacheTarget
 
-def get_path(filename):
-    return os.path.dirname(os.path.realpath(__file__)) + '/' + filename
+class Target(ApacheTarget):
 
-name = "Horde 3.3.12"
-application_dir_mapping = [get_path("application"), "/var/www"]
-database_filename = get_path("database.sql")
-database_name = "horde"
-chroot_environment = "Debian7"
+    def get_path(filename):
+        return os.path.dirname(os.path.realpath(__file__)) + '/' + filename
+    
+    name = "Horde 3.3.12"
+    application_dir_mapping = [get_path("application"), "/var/www"]
+    database_filename = get_path("database.sql")
+    database_name = "horde"
+    chroot_environment = "Debian7"
