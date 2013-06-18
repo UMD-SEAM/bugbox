4f0f0d781286f7fe0687d8a12f5e996dbe8b1c78
Julia Deutsch <juliadeutsch@aol.com>
2013-06-18 16:31:38 -0400

    apache

---
+++ framework/Targets/apachetarget.py
@@ -0,0 +1,82 @@
+import os
+from framework.Targets import Target
+from framework.Targets import TargetPluginNotFound
+
+
+class ApacheTarget(Target):
+
+    name = None
+    application_dir_mapping = []
+    database_filename = None
+    database_name = None
+    plugins = []
+    plugin_src = None
+    plugin_dest = None
+    chroot_environment = None
+    application_dir = None
+
+    def __init__(self):
+        Target.__init__(self)
+        return
+
+    def get_path(self, filename):
+	raise NotImplementedError("get_path not implemented")
+
+    def get_plugin(self, plugin_name):
+        
+        try:
+            for name, src, dest in self.plugins:
+                if name == plugin_name:
+                    return (src, dest)
+        except AttributeError:
+            pass
+        
+        raise TargetPluginNotFound(plugin_name)
+
+
+    def set_plugin(self, plugin_name):
+        self.plugin_src, self.plugin_dest = self.get_plugin(plugin_name)
+        return
+
+    def get_start_service_script(self, target_system_dir):
+
+	if not (target_system_dir and 
+		self.application_dir_mapping):
+	    raise ValueError
+
+	start_script = ["chown www-data %s%s/%s"              %(target_system_dir,
+								self.application_dir_mapping[1],
+								self.application_dir),
+			"chgrp www-data %s%s/%s"              %(target_system_dir,
+								self.application_dir_mapping[1],
+								self.application_dir),
+			"chroot %s /etc/init.d/apache2 start" %(target_system_dir,),
+			"while [ \"`pgrep apache2`\" = \"\" ]; do sleep 0.5; done;"] # wait for apache
+
+        if self.database_name:
+            start_script += ["mysql -u root -pconnection452 %s < %s" %(self.database_name,
+                                                                       self.database_filename)]
+
+        if self.plugin_src: 
+            start_script += ["mkdir %s/%s"            %(target_system_dir,
+                                                        self.plugin_dest),
+                             #"mount -o bind %s %s/%s" %(self.plugin_src,
+                             "cp -pR %s/* %s/%s"      %(self.plugin_src,
+                                                        target_system_dir,
+                                                        self.plugin_dest)]
+
+	return start_script
+
+    def get_stop_service_script(self, target_system_dir):
+	
+	if not (target_system_dir):
+	    raise ValueError
+
+	stop_script = ["chroot %s /etc/init.d/apache2 stop" %(target_system_dir,),
+		       "while pgrep \"apache2\">/dev/null; do sleep 0.5; done;"]
+
+	if self.plugin_src: 
+	    stop_script += ["rm -rf %s/%s"  %(target_system_dir,
+					      self.plugin_dest)]
+	return stop_script
+
