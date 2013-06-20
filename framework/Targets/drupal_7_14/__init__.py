990dea68a3d72c7018960009b9605e77b2996dce
Gary Nilson <gnilson@gmail.com>
2013-06-20 10:14:39 -0400

    adding target Drupal 7.14

---
+++ framework/Targets/drupal_7_14/__init__.py
@@ -0,0 +1,13 @@
+import os
+from framework.Targets import ApacheTarget
+
+class Target(ApacheTarget):
+
+    def get_path(filename):
+        return os.path.dirname(os.path.realpath(__file__)) + '/' + filename
+    
+    name = "Drupal 7.14"
+    application_dir_mapping = [get_path("application"), "/var/www"]
+    database_filename = get_path("database.sql")
+    database_name = "drupal_7_14"
+    chroot_environment = "Debian7"
