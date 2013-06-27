14581efe2f47664fb7ae8ad761097cb249d1ae67
Will Heckert <heck2@umbc.edu>
2013-06-27 14:40:13 -0400

    updated password for wordpress 2.8.3

--- accounts.py
+++ accounts.py
@@ -1,6 +1,6 @@
 
 wordpress_2_8_3_username = "admin"
-wordpress_2_8_3_password = "l!1v1s0Xz%5x"
+wordpress_2_8_3_password = "wpadminpw21"
 
 wordpress_3_8_username = "wpadmin"
 wordpress_3_8_password = "wpadminpw21"
