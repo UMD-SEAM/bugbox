3ca8ec6e0e9117ac6efc0ba25195f038136104e1
Gary Nilson <gnilson@gmail.com>
2013-06-28 12:35:05 -0400

    frame for tests created

---
+++ testbb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import unittest
+from tests import exploit_verification
+
+def main():
+    loader = unittest.TestLoader()
+    tests = loader.loadTestsFromModule(exploit_verification)
+    print(tests)
+    testRunner = unittest.runner.TextTestRunner()
+    testRunner.run(tests)
+    print('yay')
+
+if __name__ == "__main__":
+    main()
