ea4531acc3948873d1959ecd4b620e7a156dae43
Kent Wills <kentwills@aol.com>
2013-07-02 18:03:09 -0400

    testing framework

--- testbb.py
+++ testbb.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python
 import unittest
-from tests import exploit_verification
+from tests import ExploitVerification;
+import bblogger
+
+import logging 
+
+logging = logging.getLogger("testbb")
 
 def main():
+
+    suite = unittest.TestSuite()
     loader = unittest.TestLoader()
-    tests = loader.loadTestsFromModule(exploit_verification)
-    print(tests)
+    
+    for exploit in Query().exploits:
+        # logger.info("starting exploit %s", Exploit.attributes['Name'])
+        suite.addTest(ExploitVerification(exploit))
+        
     testRunner = unittest.runner.TextTestRunner()
-    testRunner.run(tests)
-    print('yay')
+    testRunner.run(suite)
     
 if __name__ == "__main__":
     main()
