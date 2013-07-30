b56642810bbe4a95c28faeeefc6a5d7271e05468
Gary Nilson <gnilson@gmail.com>
2013-07-30 09:44:36 -0400

    adding chroot_env build scripts to repo

---
+++ framework/chroot_envs/build/build_patches.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CHROOT_JAILS=(Debian5 Debian7)
+FOLDERS=(apache2 php5)
+
+for JAIL  in ${CHROOT_JAILS[@]}
+do
+    if [ -e $JAIL.patch ]; then
+	rm $JAIL.patch
+    fi
+	
+    for FOLDER in ${FOLDERS[@]}
+    do
+	diff -r -c $JAIL/etc/$FOLDER ~/vulncorpus/framework/chroot_envs/$JAIL/etc/$FOLDER >> $JAIL.patch
+    done
+    
+done
---
+++ framework/chroot_envs/build/install_chroot_jail.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+INSTALL_ROOT=/usr/lib/bugbox/framework/chroot_envs/build
+
+case "$1" in
+    Debian5)
+	TARGET_DIR=$INSTALL_ROOT/Debian5
+	PACKAGES_FILE=$INSTALL_ROOT/debian5_packages.txt
+	PATCH_FILE=$INSTALL_ROOT/Debian5.patch
+	DISTRIBUTION=lenny
+	MIRROR=http://archive.debian.org/debian
+    ;;
+    Debian7)
+	TARGET_DIR=$INSTALL_ROOT/Debian7
+	PACKAGES_FILE=$INSTALL_ROOT/debian7_packages.txt
+	PATCH_FILE=$INSTALL_ROOT/Debian7.patch
+	DISTRIBUTION=wheezy
+	MIRROR=http://debian.lcs.mit.edu/debian/
+    ;;
+    *)
+	echo "unknown debian target \`$1'" >&2
+	exit 1
+    ;;
+esac
+
+echo "Creating $TARGET_DIR directory"
+mkdir $TARGET_DIR
+echo "Building $DISTRIBUTION chroot jail"
+debootstrap $DISTRIBUTION $TARGET_DIR $MIRROR
+ret=$?
+if [[ $ret != 0 ]] ; then
+    echo "debootstrap failed to build chroot $DISTRIBUTION"
+    exit $ret
+fi    
+
+# prevent services from starting upon installation
+echo "#!/bin/sh" > $TARGET_DIR/usr/sbin/policy-rc.d
+echo "exit 101" >> $TARGET_DIR/usr/sbin/policy-rc.d
+chmod +x $TARGET_DIR/usr/sbin/policy-rc.d
+
+echo "Mounting /dev, /dev/pts, /proc"
+mount --bind /dev $TARGET_DIR/dev
+ret1=$?
+mount --bind /dev/pts $TARGET_DIR/dev/pts
+ret2=$?
+mount --bind /proc $TARGET_DIR/proc
+ret3=$?
+if [[ ($ret1 != 0) || ($ret2 != 0) || ($ret3 != 0) ]] ; then
+    echo "failed to mount!"
+    umount $TARGET_DIR/proc
+    umount $TARGET_DIR/dev/pts
+    umount $TARGET_DIR/dev
+    exit $ret1|$ret2|$ret3
+fi
+
+echo "Running apt-get update"
+chroot $TARGET_DIR apt-get update
+ret=$?
+if [[ $ret != 0 ]] ; then
+    echo "apt-get failed to update"
+    exit $ret
+fi
+
+
+echo "Installing required packages"
+chroot $TARGET_DIR apt-get --yes install `cat $PACKAGES_FILE`
+ret=$?
+if [[ $ret != 0 ]] ; then
+    echo "apt-get failed to install packages"
+    exit $ret
+fi
+
+rm $TARGET_DIR/usr/sbin/policy-rc.d 
+
+echo "Unmounting /dev, /dev/pts, /proc"
+umount $TARGET_DIR/proc
+umount $TARGET_DIR/dev/pts
+umount $TARGET_DIR/dev
+
+if [ -e $PATCH_FILE ]
+then
+    echo "Applying patch file $PATCH_FILE"
+    
+fi
