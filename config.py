a318e13a260966576e4952a96f021ec0b6245237
Gary Nilson <gnilson@gmail.com>
2013-08-01 08:21:10 -0400

    adding missing files to repo

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 # Some framework configurations
 
 # The location where the chroot environments are stored
-chroot_dirs = "/root/vulncorpus/framework/chroot_envs"
+chroot_dirs = "/usr/lib/bugbox/framework/chroot_envs"
 
 # Where the environments will be mounted when running a target service
-live_systems_dir = "/root/vulncorpus/live_systems"
+live_systems_dir = "/usr/lib/bugbox/live_systems"
 
 # The location of any xdebug configuration files
-xdebug_dir =  "/root/vulncorpus/framework/xdebug"
+xdebug_dir =  "/usr/lib/bugbox/framework/xdebug"
 
 # The desired location of xdebug traces
-traces_dir = "/root/vulncorpus/traces"
+traces_dir = "/usr/lib/bugbox/traces"
